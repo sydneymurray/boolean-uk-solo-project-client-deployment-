79dbbec01fb072d8dd777ab5866fc430f3b4dc08
sydney.murray <sydney_i_murray@yahoo.co.uk>
2021-09-29 11:56:30 +0100

    Build: Removed Unused variebles

--- src/components/CreateNewAccountModal.jsx
+++ src/components/CreateNewAccountModal.jsx
@@ -1,16 +1,10 @@
 import "../styles/CreatNewAccountModal.css"
 import {useHistory} from "react-router-dom"
-import {accountsURL} from "./data";
 import  useStore from "../hooks/useStore"
-import {useEffect, useState} from "react"
-import makePayment from "./makePayment"
-import {accountStatementURL} from "./data"
 import createNewAccount from "./createNewAccount";
 
 export default function ModalPopUp() {
   let history = useHistory()  
-  let setAccountStatement = useStore(store => store.setAccountStatement)
-  let accountStatement = useStore(store => store.accountStatement)
   const setModal = useStore(store => store.setModal)
   let loggedInCustomer = useStore(store => store.loggedInCustomer)
   if (!loggedInCustomer) return <></>
