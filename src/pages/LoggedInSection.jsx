18af7d67e831b9f8a345ce2ea86d4978e9d95101
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-02-03 18:03:15 +0000

    zz

--- src/pages/LoggedInSection.jsx
+++ src/pages/LoggedInSection.jsx
@@ -2,13 +2,16 @@ import "../styles/LoggedInSection.css"
 //import AccountsSideBar from "../components/AccountsSideBar"
 //import PaymentSideBar from "../components/PaymentSideBar"
 //import StatementSection from "../components/StatementSection"
-//import useStore from "../hooks/useStore"
+import useStore from "../hooks/useStore"
 //import {useEffect} from "react"
 
 export default function LoggedInSection(){
-  //let retrieveAccounts = useStore(store => store.retrieveAccounts)
+  let retrieveAccounts = useStore(store => store.retrieveAccounts)
   //useEffect(retrieveAccounts)
-
+  let dummy = null
+  dummy = retrieveAccounts 
+  retrieveAccounts  = dummy
+  
   return <>
     <section className="loggedInsection-container">
 
