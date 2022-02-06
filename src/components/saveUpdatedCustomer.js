78f7cfb321b41eb04d789b82caf0a100e70e050c
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-02-06 23:36:01 +0000

    Updates

--- src/components/saveUpdatedCustomer.js
+++ src/components/saveUpdatedCustomer.js
@@ -1,5 +1,25 @@
 import {updateCustomerURL} from "./data.js"
 
+export default function saveUpdatedCustomer(updatedCustomer, setModal, loggedInCustomer, setLoggedInCustomer){
+  fetch(updateCustomerURL,{
+    credentials: "include",
+    method:'PATCH',
+    headers:{'Content-Type': 'Application/json'},
+    body: JSON.stringify(updatedCustomer)
+  })
+  .then(resp=>{
+    if (resp.ok) {
+      setModal("")
+      alert(`Update Successful`)
+      updatedCustomer.customerID = loggedInCustomer.customerID
+      setLoggedInCustomer(updatedCustomer)
+    } else alert("Update unsuccessful. Try a different username")
+  })
+}
+
+/*
+import {updateCustomerURL} from "./data.js"
+
 export default function saveUpdatedCustomer(updatedCustomer, setModal, retrieveLoggedInCustomer){
   fetch(updateCustomerURL,{
     credentials: "include",
@@ -16,3 +36,4 @@ export default function saveUpdatedCustomer(updatedCustomer, setModal, retrieveL
   })
 }
 
+*/
