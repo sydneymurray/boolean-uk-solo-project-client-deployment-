e94ec007531438f187adfe3dd9705128d7bb1a99
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-03-02 11:38:42 +0000

    Working on Loan Modal

--- src/pages/ModalContainer.jsx
+++ src/pages/ModalContainer.jsx
@@ -7,7 +7,7 @@ import CustomerDetailsModal from "../components/CustomerDetailsModal"
 import PaymentModal from "../components/PaymentModal"
 import WithdrawModal from "../components/WithdrawModal"
 import CreateNewAccountModal from "../components/CreateNewAccountModal"
-import LoanModal from "../../components/LoanModal"
+import LoanModal from "../components/LoanModal"
 
 const modals = {
   "": null,
