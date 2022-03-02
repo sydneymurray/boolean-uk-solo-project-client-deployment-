fa9f2cbd516d30257efb8b4587f2626049117c49
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-03-02 10:12:12 +0000

    I put the current time in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import "../styles/Footer.css"
+import {useState, useEffect} from "react"
 
 export default function Footer(){
+  const [sigTime, setSigTime] = useState()
+  
+  useEffect(() => setInterval(() => setSigTime((new Date).toString().slice(15,24)), 1000), [])  
+
   return <>
     <div className="Footer">
       <div className="signature">
         <h4>&copy; Safe-Bank</h4>
+        <span className="sig-time">{sigTime}</span>
       </div>
       <div>
         <h4>Contact us:</h4>
@@ -21,6 +27,4 @@ export default function Footer(){
     </div>
   </>
  }
-
-
 
