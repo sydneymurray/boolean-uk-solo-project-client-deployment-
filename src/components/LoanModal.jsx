b78bf9ddc97d382f58846b588f0e1300cbf90325
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-03-02 12:19:58 +0000

    Working on Loan Modal

--- src/components/LoanModal.jsx
+++ src/components/LoanModal.jsx
@@ -4,15 +4,14 @@ import  useStore from "../hooks/useStore";
 
 export default function ModalPopUp() {
   const setModal = useStore((store) => store.setModal)
-  console.log("Loan Moadal Selected")
 
   return <>
-    <form className="modal-background">
+    <form className="modal-background" onClick={() => setModal("")}>
       <div className="modal">
         <h1 className="not-implemented-h1">NOT YET IMPLEMENTED</h1>
 
         <button type="submit" className="modalregisterbutton">
-          Update</button>
+          Return</button>
         <img className="modal-close" src="./close-button.svg" onClick={() => setModal("")} alt=""/>
       </div>
     </form>
